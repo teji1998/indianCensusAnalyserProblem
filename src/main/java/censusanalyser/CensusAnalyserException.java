74e723dfbf2898333d0d842b1d0a4d34f2a9dec7
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 09:02:38 +0530

    Added:function to load information from CSV file.

---
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -0,0 +1,15 @@
+package censusanalyser;
+
+public class CensusAnalyserException extends Exception {
+
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}

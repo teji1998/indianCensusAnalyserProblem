8c9dc02ca8fed246b896c04867e7ce75fc5f16f1
“teji1998” <tejasvinirpk@gmail.com>
2020-08-20 06:53:40 +0530

    Added:Function to load information of india state code CSV file.

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -33,4 +33,28 @@ public class CensusAnalyser {
                     CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER);
         }
     }
+
+    public int loadIndiaStateCode(String csvFilePath) throws CensusAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+
+            Iterator<IndiaStateCodeCSV> censusCSVIterator = csvToBean.iterator();
+            int numberOfEntries = 0;
+            while (censusCSVIterator.hasNext()) {
+                numberOfEntries++;
+                IndiaStateCodeCSV censusData = censusCSVIterator.next();
+            }
+            return numberOfEntries;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER);
+        }
+    }
 }

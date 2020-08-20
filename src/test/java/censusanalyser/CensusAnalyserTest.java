2e8fd2f1f6f02a5caf784f3cd04a9b1605991d33
“teji1998” <tejasvinirpk@gmail.com>
2020-08-20 05:39:35 +0530

    Added:Test case for invalid headers.

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -9,7 +9,9 @@ public class CensusAnalyserTest {
     private static final String WRONG_CSV_FILE_PATH =  "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE_PATH =  "./src/test/resources/IndiaStateCensusData.txt";
     private static final String INVALID_DELIMITER_FILE_PATH =  "./src/test/resources/invalidDelimitersIndiaStateCensusData.csv";
+    private static final String INVALID_HEADER_FILE_PATH =  "./src/test/resources/invalidHeadersIndiaStateCensusData.csv";
 
+    //To obtain the number of record
     @Test
     public void givenIndianCensusDataCSVFileReturnsCorrectRecords() {
         try {
@@ -19,6 +21,7 @@ public class CensusAnalyserTest {
         } catch (CensusAnalyserException e) { }
     }
 
+    //To test for a wrong file path
     @Test
     public void givenIndianCensusDataCSVFile_whenWithWrongPath_shouldThrowException() {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -31,6 +34,7 @@ public class CensusAnalyserTest {
         }
     }
 
+    //To test for a wrong file type
     @Test
     public void givenIndianCensusDataCSVFile_whenWithWrongFileType_shouldThrowException() {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -43,6 +47,7 @@ public class CensusAnalyserTest {
         }
     }
 
+    //To test for invalid delimiters
     @Test
     public void givenIndianCensusDataCSVFile_whenWithWrongDelimiters_shouldThrowException() {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
@@ -50,6 +55,19 @@ public class CensusAnalyserTest {
         exceptionRule.expect(CensusAnalyserException.class);
         try {
             censusAnalyser.loadIndiaCensusData(INVALID_DELIMITER_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER, e.type);
+        }
+    }
+
+    //To test for invalid headers
+    @Test
+    public void givenIndianCensusDataCSVFile_whenWithWrongHeaders_shouldThrowException() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        ExpectedException exceptionRule = ExpectedException.none();
+        exceptionRule.expect(CensusAnalyserException.class);
+        try {
+            censusAnalyser.loadIndiaCensusData(INVALID_HEADER_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER, e.type);
         }
